26b38bdad7a21c5e8ac0a043f3726a37ff9b522a
AbdullahAli2005 <smabd7409@gmail.com>
2025-12-23 21:10:17 +0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js';
+import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
+import analyticsRoutes from './routes/analytics.route.js';
+
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,  // important if you plan to use auth cookies/tokens later
+}));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+    console.error("Unhandled error:", err);
+    res.status(500).json({ message: "Server error" });
+});
