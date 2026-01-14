49a02fbc24b0624e2f5fb079af38dfcb5fba9306
AbdullahAli2005 <smabd7409@gmail.com>
2026-01-14 17:16:22 +0500

    url updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === 'development' ? 'http://localhost:5000/api' : 'http://localhost:5000/api',
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://buyora-production.up.railway.app/api",
     withCredentials: true,
 });
 
